5cfe9e3955616fd033716e259fbff120c7c94181
Alla Marchenko <marchenko_alla@yahoo.com>
2019-07-21 17:02:41 +0200

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,1 +1,13 @@
-import Tkinter as tk
+import tkinter as tk
+
+class App(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.master.title ('Hello world')
+
+        tk.Label(self, text = 'This is your first GUI. (high five)').pack()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = App(root)
+    app.mainloop()
