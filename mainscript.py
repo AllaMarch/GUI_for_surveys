f10116aa7cd91d1b7cbeefefdedf6f4fe47fcc48
Alla Marchenko <marchenko_alla@yahoo.com>
2019-07-21 16:53:25 +0200

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,5 +1,1 @@
-import tkinter
-window = tkinter.Tk()
-#renameing the window
-window.title('Testcell Experiment')
-label = tkinter.Label(window, text = 'Welcome to the Testcell Experiment')
+import Tkinter as tk
