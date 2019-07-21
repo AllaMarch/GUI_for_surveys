89ecf4185525ea1d58d4e724b866a2481bcda728
Alla Marchenko <marchenko_alla@yahoo.com>
2019-07-21 17:41:01 +0200

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import subprocess
+
+
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -6,8 +9,18 @@ class App(tk.Frame):
         self.pack()
         self.master.title ('Hello world')
 
+        self.master.resizable(False, False)
+        tk.Button(self, text = 'OK', default = 'active', command = self.click_ok).pack(side = 'right')
+        #self.master.tk_setPallete(background = '#ececec')
+
+        #x = (self.master.winfo_screenwidth()-self.master.winfo_reqwidth())/2
+        #y = (self.master.winfo_screenheight()-self.master.winfo_reqheight())/3
+        #self.master.winfo.geometry('+{}+{}'.format(x,y))
+        #self.master.config(menu=tk.Menu(self.master))
+
         tk.Label(self, text = 'This is your first GUI. (high five)').pack()
 if __name__ == '__main__':
+#    subprocess.cell(['/user/bin/osascript', '-e', 'tell app "Finder" to set fromntmost of subprocess 'Python' to true'])
     root = tk.Tk()
     app = App(root)
     app.mainloop()
